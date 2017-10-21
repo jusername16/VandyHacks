862a354af6005cec9f88ff30e0a56916986c9868
Shubham-Gupte <shubhamgupte01@gmail.com>
2017-10-21 15:07:54 -0500

    Finished location with Usher

--- UsherAuth/location.py
+++ UsherAuth/location.py
@@ -1,6 +1,11 @@
 import urllib, json,requests
-def main():
-    UsherLocation = "Kissam College Hall".replace(" ", "+")
+def locationVerify():
+    requestURL = "https://env-72355.customer.cloud.microstrategy.com:2443/allusers?info_type=ADAttrs&badge_index_from=1&badge_id=55&target_badge_ids=55&ad_attr_list=lat,lon&access_token=POn5F2VXQdJRo9yUGTR4xIB8qnA1M-pP5EPNwkjRkLIcs--nfDi6FEBPIiDIsyXmzlY"
+    r = requests.get(requestURL)
+    jdata = r.json()
+    lat = jdata['ad_attr_list'][0]['lat']
+    lon = jdata['ad_attr_list'][0]['lon']
+    UsherLocation = str(lat) + "," + str(lon)
     EchoLocation = "The Wondry".replace(" ", "+")
     API_KEY = "AIzaSyAJTw_aMHrgAt9uBBIOGoZnAjWtdKu_kVw"
     requestURL = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + EchoLocation + "&destinations=" + UsherLocation + "&key=AIzaSyAJTw_aMHrgAt9uBBIOGoZnAjWtdKu_kVw"
@@ -11,9 +16,9 @@ def main():
         distanceBetween = float(distanceBetween.replace(" ft", "")) * 0.000189393939
     distanceBetween = str(distanceBetween).replace(" mi", "")
     if float(distanceBetween) < 1:
-        print "You're In"
+        return True
     else:
-        print "You're not Verified"
+        return False
 
 if  __name__ =='__main__':
-    main()
+    locationVerify()
